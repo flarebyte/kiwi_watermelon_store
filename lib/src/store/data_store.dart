81e3509598028feb87194341c8378825053f9ebc
Olivier Huin <olih@users.noreply.github.com>
2025-07-05 15:18:30 +0100

    BaseStringDataStore extends BaseTypedDataStore<String>

--- lib/src/store/data_store.dart
+++ lib/src/store/data_store.dart
@@ -22,22 +22,6 @@ abstract class BaseTypedDataStore<T> {
 
 /// Represents a key-value data store that supports prefix validation
 /// and optional typed value decoding.
-abstract class BaseStringDataStore {
-  /// Retrieves the string value associated with [key], or `null` if not found.
-  String? get(String key);
-
-  ///The value for the given key, or null if key is not in the map.
-  String? operator [](String key);
-
-  /// Associates the [value] with the given [key], replacing any existing value.
-  void set(String key, String value);
-
-  /// Removes the entry associated with [key], if it exists.
-  void remove(String key);
-
-  /// Clear the data store
-  void clear();
-
-  /// Add multiple values at once
-  void addAll(Map<String, String> other);
+abstract class BaseStringDataStore extends BaseTypedDataStore<String> {
+  
 }
