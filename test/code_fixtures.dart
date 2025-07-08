80b3389f5dfaec87ffc5ddadca3de4d530a22ee2
Olivier Huin <olih@users.noreply.github.com>
2025-07-08 21:26:32 +0100

    Adds list and set commands

--- test/code_fixtures.dart
+++ test/code_fixtures.dart
@@ -37,4 +37,60 @@ class QuickQueryMaker {
     return "DEL $values";
   }
 
+// List Commands
+  String lpush(String name, List<Object> values) {
+    final joined = values.join(' ');
+    return 'LPUSH $prefix:$name $joined';
+  }
+
+  String rpush(String name, List<Object> values) {
+    final joined = values.join(' ');
+    return 'RPUSH $prefix:$name $joined';
+  }
+
+  String lrem(String name, int count, Object value) {
+    return 'LREM $prefix:$name $count $value';
+  }
+
+  String ltrim(String name, int start, int stop) {
+    return 'LTRIM $prefix:$name $start $stop';
+  }
+
+  String rpoplpush(String source, String destination) {
+    return 'RPOPLPUSH $prefix:$source $prefix:$destination';
+  }
+
+  String lmove(String source, String destination, String from, String to) {
+    return 'LMOVE $prefix:$source $prefix:$destination $from $to';
+  }
+
+  // Set commands
+
+  String sadd(String name, List<Object> members) {
+    final joined = members.join(' ');
+    return 'SADD $prefix:$name $joined';
+  }
+
+  String srem(String name, List<Object> members) {
+    final joined = members.join(' ');
+    return 'SREM $prefix:$name $joined';
+  }
+
+  String smove(String source, String destination, Object member) {
+    return 'SMOVE $prefix:$source $prefix:$destination $member';
+  }
+
+  // Keys command
+
+  String rename(String oldKey, String newKey) {
+    return 'RENAME $prefix:$oldKey $prefix:$newKey';
+  }
+
+  String renamenx(String oldKey, String newKey) {
+    return 'RENAMENX $prefix:$oldKey $prefix:$newKey';
+  }
+
+  String flushDb() {
+    return 'FLUSHDB';
+  }
 }
