90666c1b1ec30093618a0999d99956faf0bf8a8f
Olivier Huin <olih@users.noreply.github.com>
2025-07-12 21:11:08 +0100

    More test cov for command analyser

--- test/language/command_analyser_test.dart
+++ test/language/command_analyser_test.dart
@@ -11,22 +11,82 @@ import '../code_fixtures.dart';
 void main() {
   final tokeniser = KiwiWatermelonTokeniser();
   final KiwiCommandAnalyser analyser =
-        KiwiCommandAnalyser(options: storeOptions);
+      KiwiCommandAnalyser(options: storeOptions);
   KiwiWatermelonAction parseSingleCommand(String code) {
     final tokens = tokeniser.parse(code);
     final tokenStream = KiwiWatermelonTokenStream(tokens);
     return analyser.parseSingleCommand(tokenStream);
   }
+
   group('INCR analyser', () {
-    
     test('should parse command', () {
-      final action = parseSingleCommand('INCR env:metrics:api:total:color:blue:status:ready');
-      expect(action,  isA<KiwiIncrAction>());
+      final action = parseSingleCommand(
+          'INCR env:metrics:api:total:color:blue:status:ready');
+      expect(action, isA<KiwiIncrAction>());
     });
 
-     test('throws SemanticException when incorrect', () {
-      expect(
-          () => parseSingleCommand('INCR 12'),
+    test('throws SemanticException when incorrect', () {
+      expect(() => parseSingleCommand('INCR 12'),
+          throwsA(isA<KiwiWatermelonSemanticException>()));
+    });
+  });
+
+  group('DECR analyser', () {
+    test('should parse command', () {
+      final action = parseSingleCommand('DECR env:retries');
+      expect(action, isA<KiwiDecrAction>());
+    });
+
+    test('throws SemanticException on invalid key', () {
+      expect(() => parseSingleCommand('DECR ,'),
+          throwsA(isA<KiwiWatermelonSemanticException>()));
+    });
+  });
+
+  group('INCRBY analyser', () {
+    test('should parse command', () {
+      final action = parseSingleCommand('INCRBY env:views 10');
+      expect(action, isA<KiwiIncrByAction>());
+    });
+
+    test('throws SemanticException on non-integer', () {
+      expect(() => parseSingleCommand('INCRBY env:views ten'),
+          throwsA(isA<KiwiWatermelonSemanticException>()));
+    });
+  });
+
+  group('DECRBY analyser', () {
+    test('should parse command', () {
+      final action = parseSingleCommand('DECRBY env:count 5');
+      expect(action, isA<KiwiDecrByAction>());
+    });
+
+    test('throws SemanticException on missing value', () {
+      expect(() => parseSingleCommand('DECRBY env:count'),
+          throwsA(isA<KiwiWatermelonSemanticException>()));
+    });
+  });
+
+  group('INCRBYFLOAT analyser', () {
+    test('should parse command', () {
+      final action = parseSingleCommand('INCRBYFLOAT env:price 3.14');
+      expect(action, isA<KiwiIncrByFloatAction>());
+    });
+
+    test('throws SemanticException on non-float', () {
+      expect(() => parseSingleCommand('INCRBYFLOAT env:price free'),
+          throwsA(isA<KiwiWatermelonSemanticException>()));
+    });
+  });
+
+  group('DECRBYFLOAT analyser', () {
+    test('should parse command', () {
+      final action = parseSingleCommand('DECRBYFLOAT env:budget 1.25');
+      expect(action, isA<KiwiDecrByFloatAction>());
+    });
+
+    test('throws SemanticException on malformed number', () {
+      expect(() => parseSingleCommand('DECRBYFLOAT env:budget --1.0'),
           throwsA(isA<KiwiWatermelonSemanticException>()));
     });
   });
