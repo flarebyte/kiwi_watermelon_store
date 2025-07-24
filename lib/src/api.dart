bab0363b4304b5e53a2410072b7fc45db7359e66
Olivier Huin <olih@users.noreply.github.com>
2025-07-24 09:26:44 +0100

    more factory in api

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -1,6 +1,9 @@
 import 'autorisation/capability.dart';
+import 'factory.dart';
 import 'language/syntax_analyser.dart';
 import 'language/tokeniser.dart';
+import 'listener/handler.dart';
+import 'session_manager.dart';
 import 'store/manager_options.dart';
 
 class KiwiWatermelonApi {
@@ -23,14 +26,25 @@ class KiwiWatermelonApi {
   static bool _defaultValidator(String name) =>
       RegExp(r'^[a-zA-Z][a-zA-Z0-9_:]*$').hasMatch(name);
 
-  /// create a Tokeniser
+  /// Constructs a [KiwiWatermelonTokeniser]
   static KiwiWatermelonTokeniser createTokeniser() {
     return KiwiWatermelonTokeniser();
   }
 
-  /// create a syntax analyser
-  static createSyntaxAnalyser(
+  /// Constructs a [KiwiWatermelonSyntaxAnalyser]
+  static KiwiWatermelonSyntaxAnalyser createSyntaxAnalyser(
       {required KiwiWatermelonOptions options, required String role}) {
     return KiwiWatermelonSyntaxAnalyser(options: options, role: role);
+  }
+
+  /// Constructs a [KiwiWatermelonSessionManager]
+  static KiwiWatermelonSessionManager createSessionManager(
+      {required KiwiWatermelonOptions options,
+      required KiwiWatermelonBaseFactory? factory,
+      required KiwiWatermelonOnViewUpdate? onUpdate}) {
+    return KiwiWatermelonSessionManager(
+        options: options,
+        factory: factory ?? KiwiWatermelonDefaultFactory(),
+        onUpdate: onUpdate);
   }
 }
