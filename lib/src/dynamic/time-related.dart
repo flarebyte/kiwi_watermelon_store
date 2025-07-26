f690f79a8c615d736363054b60de1553f8c98e62
Olivier Huin <olih@users.noreply.github.com>
2025-07-26 16:49:58 +0100

    Adds time related

---
+++ lib/src/dynamic/time-related.dart
@@ -0,0 +1,9 @@
+import '../../kiwi_watermelon_store.dart';
+
+class KiwiWatermelonDate extends BaseGetTypedDataStore<String> {
+  @override
+  String? get(String key) {
+    // TODO date:day, date:month, date:year, time:hour
+    return null;
+  }
+}
