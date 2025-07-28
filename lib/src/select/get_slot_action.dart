b19f47ee64a1dc86f2c364517b7df4320fc48bde
Olivier Huin <olih@users.noreply.github.com>
2025-07-28 07:25:51 +0100

    KiwiGetSlotIndexSAction

---
+++ lib/src/select/get_slot_action.dart
@@ -0,0 +1,22 @@
+import 'package:kiwi_watermelon_store/src/select/select_action_result.dart';
+
+import 'package:kiwi_watermelon_store/src/store/data_store.dart';
+
+import 'select_base_action.dart';
+
+/// get a string
+/// GETSLOTINDEX <value> <min_value> <max_value> <num_slots>
+class KiwiGetSlotIndexSAction extends KiwiWatermelonSelectQuery<String> {
+  final String key;
+  final int minValue;
+  final int maxValue;
+  final int numSlots;
+  KiwiGetSlotIndexSAction(this.key, this.minValue, this.maxValue, this.numSlots);
+
+  @override
+  KiwiWatermelonSelectActionResult<String> execute(
+      BaseGetTypedDataStore<String> store) {
+        // TODO
+    return KiwiWatermelonSelectActionResult(value: store.get(key));
+  }
+}
